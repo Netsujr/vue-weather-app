b9750b72b00ed33ced4dc2fd6c87dde72e0376f9
Renato Delboni <renatonato_jr@hotmail.com>
2022-03-25 11:42:55 +0900

    search works

--- src/App.vue
+++ src/App.vue
@@ -2,12 +2,18 @@
   <div id="app">
     <main>
       <div class="search-box">
-        <input type="text" placeholder="Search..." id="" class="search-bar" />
+        <input
+          type="text"
+          placeholder="Search..."
+          class="search-bar"
+          v-model="query"
+          @keypress="fetchWeather"
+        />
       </div>
 
-      <div class="weather-wrap">
+      <div class="weather-wrap" v-if="typeof weather.main != 'undefined'">
         <div class="location-box">
-          <div class="location">London</div>
+          <div class="location">{{ weather.name }}, {{ weather.sys.country }}</div>
           <div class="date">Monday</div>
         </div>
         <div class="weather-box">
@@ -28,8 +34,19 @@ export default {
       url_base: "https://api.openweathermap.org/data/2.5/",
       query: "",
       weather: {},
-
     };
+  },
+  methods: {
+    fetchWeather(e) {
+      if (e.key == 'Enter') {
+        const url = `${this.url_base}weather?q=${this.query}&units=metric&appid=${this.api_key}`;
+        fetch(url)
+          .then((response) => response.json())
+          .then((data) => {
+            this.weather = data;
+          });
+      }
+    },
   },
 };
 </script>
